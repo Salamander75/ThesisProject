d0505702f4a4005b1d07072dec89b3b85a0364db
Salamander75 <jurgensonkarlman@gmail.com>
2017-10-03 23:32:29 +0300

    Inject JavaScript only one time, when browser is opened #2

--- src/main/java/Testing.java
+++ src/main/java/Testing.java
@@ -1,21 +1,29 @@
-import org.openqa.selenium.Alert;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-
-import static com.codeborne.selenide.Selenide.sleep;
-
 
 /**
  * Created by Karl on 18.09.2017.
  */
 public class Testing extends BaseInitialization {
 
+    private JavascriptExecutor jse = (JavascriptExecutor) driver;
+
+    private String scriptId = RandomStringUtils.randomAlphanumeric(50);
+    private String script =
+            "var head= document.getElementsByTagName('head')[0];" +
+                    "var script= document.createElement('script');" +
+                    "script.id = '"+ scriptId +"'; script.type= 'text/javascript';" +
+                    "script.src = 'http://localhost/jsTest/myScript.js';" +
+                    "head.appendChild(script);" +
+                    "var css = '.highlight { background-color: yellow; }';" +
+                    "var style = document.createElement('style');" +
+                    "style.type = 'text/css';" +
+                    "style.appendChild(document.createTextNode(css));" +
+                    "head.appendChild(style);";
     public static void main(String args[]) throws InterruptedException {
         new Testing();
     }
@@ -32,41 +40,22 @@ public class Testing extends BaseInitialization {
         }
         System.out.println(javascript);
         driver.get("http://www.urbandead.com/");
-        JavascriptExecutor jse = (JavascriptExecutor) driver;
-        String script =
-                "var head= document.getElementsByTagName('head')[0];" +
-                        "var script= document.createElement('script');" +
-                        "script.type= 'text/javascript';" +
-                        "script.src = 'http://localhost/jsTest/myScript.js';" +
-                        "head.appendChild(script);" +
-                        "var css = '.highlight { background-color: yellow; }';" +
-                        "var style = document.createElement('style');" +
-                        "style.type = 'text/css';" +
-                        "style.appendChild(document.createTextNode(css));" +
-                        "head.appendChild(style);";
-        jse.executeScript(script);
-        // main loop
-        while (true) {
-            checkAlert();
-            String oldUrl = driver.getCurrentUrl();
-            Thread.sleep(1000);
-            System.out.println("test");
-            System.out.println(oldUrl);
-            if (!(oldUrl.equals(driver.getCurrentUrl())) || oldUrl.equals(driver.getCurrentUrl())) {
-                System.out.println("Url has changed");
-                jse.executeScript(script);
-            }
-        }
+        elementInspectionMainLoop();
     }
 
-    public void checkAlert() {
-        try {
-            WebDriverWait wait = new WebDriverWait(driver, 2);
-            wait.until(ExpectedConditions.alertIsPresent());
-       //     Alert alert = driver.switchTo().alert();
-      //      alert.accept();
-        } catch (Exception e) {
-            checkAlert();
+    private void elementInspectionMainLoop() {
+        while (true) {
+            try {
+                // Necessary to avoid ultra fast true loop
+                Thread.sleep(1000);
+                if (jse.executeScript("return document.getElementById('"+ scriptId +"')") == null) {
+                    System.out.println("Scripti pole olemas");
+                    jse.executeScript(script);
+                }
+            } catch(Exception e) {
+                System.out.println("Some exception wants to happen!");
+                elementInspectionMainLoop();
+            }
         }
     }
 }
